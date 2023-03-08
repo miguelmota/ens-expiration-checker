2eeae20ed50d2d7ea1aaa7b343bb608f599b58f2
Miguel Mota <hello@miguelmota.com>
2023-03-08 11:20:01 -0800

    Resolve ENS

--- README.md
+++ README.md
@@ -1,13 +1,39 @@
-# ENS Expire Checker
+# ENS Expiration Checker
 
-> Check ENS name expiration dates
+> Check [ENS](https://ens.domains/) name expiration dates
+
+This script requires an [Alchemy](https://www.alchemy.com/) API key to fetch NFT information.
+
+Create `.env`
 
 ```sh
-touch addresses.txt
+ALCHEMY_API_KEY=<your_api_key_here>
 ```
 
+Add your addresses and/or ENS names to a file:
+
 ```sh
-node index.js
+cat addresses.txt
+
+0xaB7C8803962c0f2F5BBBe3FA8bf41cd82AA1923C
+uniswap.eth
+...
+```
+
+Run the script:
+
+```sh
+node index.js addresses.txt
+```
+
+Example output
+
+```sh
+mochi.eth  2027-12-31T13:44:24.000-08:00 0xaB7C8803962c0f2F5BBBe3FA8bf41cd82AA1923C
+ethers.eth  2027-12-31T13:44:24.000-08:00 0xaB7C8803962c0f2F5BBBe3FA8bf41cd82AA1923C
+ricmoo.eth  2027-12-31T13:44:24.000-08:00 0xaB7C8803962c0f2F5BBBe3FA8bf41cd82AA1923C
+mochis.eth  2027-12-31T13:44:24.000-08:00 0xaB7C8803962c0f2F5BBBe3FA8bf41cd82AA1923C
+uniswap.eth  2035-05-04T08:18:00.000-07:00 0x1a9C8182C09F50C8318d769245beA52c32BE35BC
 ```
 
 ## License
--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const fs = require('fs')
 const path = require('path')
 const { Alchemy, Network } = require('alchemy-sdk')
 const { DateTime } = require('luxon')
+const { ethers, BigNumber, utils } = require('ethers')
 require('dotenv').config()
 
 const alchemyApiKey = process.env.ALCHEMY_API_KEY
+const ethereumRpc = process.env.ETHEREUM_RPC || 'https://rpc.ankr.com/eth'
+
+if (!ethereumRpc) {
+  throw new Error('Missing ETHEREUM_RPC environment variable')
+}
 
 if (!alchemyApiKey) {
   throw new Error('Missing ALCHEMY_API_KEY environment variable')
@@ -18,30 +24,59 @@ const settings = {
 const alchemy = new Alchemy(settings)
 
 async function main () {
-  const inputFileName = 'addresses.txt'
+  const inputFileName = process.argv[2] || 'addresses.txt'
   const inputFilePath = path.join(__dirname, inputFileName)
   const inputFileData = fs.readFileSync(inputFilePath, 'utf8')
   const addresses = inputFileData.split('\n').map((address) => address.trim()).filter((address) => address.length > 0)
 
-  const result = []
-  for (const address of addresses) {
+  const result = {}
+  for (let address of addresses) {
+    let filterName = ''
+    if (address.endsWith('.eth')) {
+      const name = address
+      filterName = name
+      const provider = new ethers.providers.StaticJsonRpcProvider(ethereumRpc)
+
+      const ensAddr = '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85'
+      const ensAbi = ['function ownerOf(uint256) view returns (address)']
+      const contract = new ethers.Contract(ensAddr, ensAbi, provider)
+      const labelHash = utils.keccak256(utils.toUtf8Bytes(name.replace('.eth', '')))
+      const tokenId = BigNumber.from(labelHash).toString()
+      address = await contract.ownerOf(tokenId) // registrant address?
+
+      // const ensAddr = (await provider.getNetwork()).ensAddress
+      // const ensAbi = [ "function owner(bytes32) view returns (address)" ]
+      // const contract = new ethers.Contract(ensAddr, ensAbi, provider)
+      // address = await contract.owner(ethers.utils.namehash(name)) // controller address?
+    }
+
     const nftsForOwner = await alchemy.nft.getNftsForOwner(address)
     for (const nft of nftsForOwner.ownedNfts) {
       if (nft.title.includes('eth')) {
         const name = nft.title
-        const expiresMs = Number(nft.rawMetadata.attributes.find(attr => attr.trait_type === 'Expiration Date').value)
+        if (filterName && filterName !== name) {
+          continue
+        }
+        const attrs = nft && nft.rawMetadata && nft.rawMetadata.attributes && nft.rawMetadata.attributes.find(attr => attr.trait_type === 'Expiration Date')
+        if (!attrs) {
+          continue
+        }
+        const expiresMs = Number(attrs.value)
+        if (!expiresMs) {
+          continue
+        }
         const expiresAt = DateTime.fromMillis(expiresMs).toISO()
-        result.push({
+        result[name] = {
           name,
           expiresMs,
           expiresAt,
           account: address
-        })
+        }
       }
     }
   }
 
-  const sorted = result.sort((a, b) => a.expiresMs - b.expiresMs)
+  const sorted = Object.values(result).sort((a, b) => a.expiresMs - b.expiresMs)
 
   const now = Date.now()
   for (const item of sorted) {
